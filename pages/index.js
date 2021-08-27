8b098786645316147bbfc0c6d03836234abe8d26
Carlos Henrique Fallac <carloshfallac@hotmail.com>
2021-08-27 20:23:15 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,33 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+const MenuWrapper = styled.nav`
+  
+`
+
+
+function Menu() {
+  return(
+    <nav>
+      <div>
+        Logo area
+      </div>
+      <div>
+        Links area
+      </div>
+      <div>
+        Buttons area
+      </div>
+    </nav>
+  )
+
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <div>
+      <Menu />
+     <Title>My page</Title>
+     </div>
+  )
 }
